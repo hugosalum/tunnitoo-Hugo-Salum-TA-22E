665fe9c251f945967a0b2c1f2e727ecbdd0c14fa
hugosalum <113598745+hugosalum@users.noreply.github.com>
2022-09-30 09:04:08 +0300

    Create tunnitöö.js

---
+++ tunnitöö.js
@@ -0,0 +1,26 @@
+z=1
+y=Math.random(1,10)
+y=y*10
+y=Math.round(y)
+console.log(y)
+for (let i=0;i < 3; i++){
+    x=prompt("Sisesta üks number 1-10-ni")
+    c=x+1
+    v=x-1
+    z=z+1
+    if (c==y){
+        prompt("Ai, sa pakkusid väga lähedale.")
+    }
+    if (v==y){
+        prompt("Ai, sa pakkusid väga lähedale.")
+    }
+    if (x==y){
+        prompt(":)")
+        break;
+    } else {
+        prompt(":(")
+    }
+    if (z==4){
+        break;
+    }
+}
